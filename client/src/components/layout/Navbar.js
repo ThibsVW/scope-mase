beacfb31e9af673019f856d18b0aa091ee7fd68b
ThibsVW <thibault.vanweydeveld@hotmail.com>
2020-10-12 15:02:56 +0800

    router-dom set up

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav class='navbar bg-dark'>
+    <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
-          <i class='fas fa-code'></i> SCOPE
-        </a>
+        <Link to='index.html'>
+          <i className='fas fa-code'></i> SCOPE
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='profiles.html'>Developers</a>
+          <Link to='!#5'>Developers</Link>
         </li>
         <li>
-          <a href='register.html'>Register</a>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <a href='login.html'>Login</a>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
