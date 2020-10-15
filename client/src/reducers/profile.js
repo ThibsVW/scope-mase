18c38a8f3368166659be290cf55ceb27fccf1311
ThibsVW <thibault.vanweydeveld@hotmail.com>
2020-10-15 14:20:12 +0800

    codes cleaned up

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -4,8 +4,6 @@ import {
   CLEAR_PROFILE,
   UPDATE_PROFILE,
   GET_PROFILES,
-  GET_REPOS,
-  NO_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -44,17 +42,6 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: null,
-        repos: [],
-      };
-    case GET_REPOS:
-      return {
-        ...state,
-        repos: payload,
-        loading: false,
-      };
-    case NO_REPOS:
-      return {
-        ...state,
         repos: [],
       };
     default:
